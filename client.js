f26ce7cb6c0ce00ffe01be22d6efd38e7ca7dc47
Good-player <very.good.lol@hotmail.com>
2025-03-05 22:37:18 +0100

    Create client.js

---
+++ client.js
@@ -0,0 +1,21 @@
+function authenticateAndEditPost() {
+    window.location.href = 'https://github.com/login/oauth/authorize?client_id=YOUR_GITHUB_CLIENT_ID&scope=repo';
+}
+
+// After authentication, capture the code from the redirect URL and send it to your server
+const urlParams = new URLSearchParams(window.location.search);
+const code = urlParams.get('code');
+if (code) {
+    fetch('/auth/github/callback', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code }),
+    }).then(response => response.json())
+      .then(data => {
+          if (data.username === 'Good-player') {
+              // Show delete/edit buttons
+          }
+      });
+}
